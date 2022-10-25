81b63738d82ca608cc513cb48d77ab13f1bfa6f8
Ian Romanick <ian.d.romanick@intel.com>
2022-10-24 23:49:06 -0700

    Use correct type for recvfrom addrlen parameter

--- linuxdoom-1.10/i_net.c
+++ linuxdoom-1.10/i_net.c
@@ -160,7 +160,7 @@ void PacketGet (void)
     int			i;
     int			c;
     struct sockaddr_in	fromaddress;
-    int			fromlen;
+    socklen_t		fromlen;
     doomdata_t		sw;
 				
     fromlen = sizeof(fromaddress);
